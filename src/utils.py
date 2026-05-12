22a30b298bfff0669bd4602bf12b71dd9544994d
燕首龙 <yanshoulong@yanshoulongdeMacBook-Pro-7.local>
2026-05-12 12:36:12 +0800

    Add argparse support

--- src/utils.py
+++ src/utils.py
@@ -2,13 +2,13 @@ import argparse
 from pathlib import Path
 
 def parse_args():
-    paser = argparse.ArgumentParser(description="短视频数据解析工具")
+    parser = argparse.ArgumentParser(description="短视频数据解析工具")
 
-    paser.add_argument(
+    parser.add_argument(
         "--input",
         type=str,
         default = "data/test_project.xlsx",
         help="上传文件路径，支持解析xlsx和csv文件"
     )
 
-    return parse_args()
+    return parser.parse_args()
